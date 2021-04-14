0dd630b1d034e255532c0fffc1bdb1ebe10973cb
rhc07 <rorieclarke@gmail.com>
2021-04-14 20:03:33 +0100

    created routes for the ejs files

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 
-
 app.get("/", (request, response) => {
-    response.send("Hello");
+    response.render("index");
+});
+
+app.get("/users/register", (request, response) => {
+    response.render("register")
+});
+
+app.get("/users/login", (request, response) => {
+    response.render("login")
+});
+
+app.get("/users/dashboard", (request, response) => {
+    response.render("dashboard")
 });
 
 app.listen(PORT, () => {
