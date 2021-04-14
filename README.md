317cad771ca647ba3c3a4cb77bc1b3fb9efe6768
sandy <pounoussamy.sandy@gmail.com>
2021-04-14 14:37:36 +0100

    Added routes for index, login, register

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # final-project
+
+Express - application server
+EJS - templating language
+
+Developement Dependancies
+nodemon - restart the server automatically when it detect changes
+dotenv - allow us to hav .env 
--- cookwars/controller/landing.js
+++
@@ -1,9 +0,0 @@
-
-exports.get_landing = function(req, res, next) {
-  res.render('index', { title: 'Express' });
-};
-
-exports.submit_lead = function(req, res, next) {
-  console.log("lead email:", req.body.lead_email)
-  res.redirect('/')
-};
