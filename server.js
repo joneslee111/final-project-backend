3497ef5891ddc333e6685fefacd755ba6397a7a0
rhc07 <rorieclarke@gmail.com>
2021-04-14 20:08:02 +0100

    the views folder can now be rendered by the server js file

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 9000;
 
+// this tells our app to render the ejs files in the views folder
+app.set("view engine", "ejs"); 
 
+// these are the app controller routes
 app.get("/", (request, response) => {
     response.render("index");
 });
