cc7828b4a58d1c1c894fa184b47658f092f35bc8
rhc07 <rorieclarke@gmail.com>
2021-04-14 22:53:41 +0100

    added bcrypt and created a post a request to fetch the params from the register route

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 9000;
 
 // this tells our app to render the ejs files in the views folder
 app.set("view engine", "ejs"); 
+// this send details from the front end to the server
+app.use(express.urlencoded({ extended: false }));
 
 // these are the app controller routes
 app.get("/", (request, response) => {
@@ -24,6 +26,20 @@ app.get("/users/login", (request, response) => {
 // set the user variable/object to myself as a placeholder. This will print my name in the dashboard views file.
 app.get("/users/dashboard", (request, response) => {
     response.render("dashboard", { user: "Rorie" })
+});
+
+// retrieving the params from the register route with a post request
+app.post("/users/register", (request, response) => {
+    let { name, email, username, password, password_confirmation } = request.body;
+
+// printing the params back to the console to see if it's returning anything - test passes!
+    console.log({
+        name,
+        email,
+        username,
+        password,
+        password_confirmation
+    });
 });
 
 app.listen(PORT, () => {
