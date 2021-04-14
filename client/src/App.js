3534d62786cb8c583e304dfd454642907c82c146
Raf Swiderski <rafswid@icloud.com>
2021-04-14 14:53:01 +0100

    npm audit report

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  };
+
+  componentWillMount() {
+    this.callAPI();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <p className="App-intro">;{this.state.apiResponse}</p>
+
+          {/* <p>
+                  Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  };
+};
 
 export default App;
