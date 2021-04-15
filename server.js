3d31462ac4f05619a911529b0e71292980cf8556
sandy <pounoussamy.sandy@gmail.com>
2021-04-15 11:31:02 +0100

    registration and authentication feature working

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require ('bcrypt')
 const passport = require('passport')
-
+const flash = require('express-flash')
+const session = require('express-session')
 const initializePassport  = require('./passport-config')
-initializePassport(passport)
+
+initializePassport(
+  passport, 
+  username => users.find(user => user.username === username),
+  id => users.find(user => user.id === id)
+)
 
 app.set('view-engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
+// the below willl be replace with a database connection
 const users = [];
 
-
+//this will have to move to a controller folder
 app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'USER' });
+  res.render('index.ejs', { name: req.user.name });
 });
 
 app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
-app.post('/login', (req, res) => {
-  
-});
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 app.get('/register', (req, res) => {
   res.render('register.ejs');
@@ -36,7 +56,8 @@ app.post('/register', async (req, res) => {
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
-      username: req.body.email,
+      username: req.body.username,
+      email: req.body.email,
       password: hashedPassword
     })
     res.redirect('/login')
